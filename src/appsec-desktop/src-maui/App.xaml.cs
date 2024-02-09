fb33fbac317984b04f706ba5f6b1678ee31a3363
loribao <loribao@hotmail.com>
2024-02-09 09:14:38 -0300

    :init: modifying the base templates UI

---
+++ src/appsec-desktop/src-maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace src_maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ src/appsec-desktop/src-maui/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace src_maui;
+
+public static class MauiProgram
+{
+	public static IServiceCollection serviceCollection;
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+		serviceCollection = builder.Services;
+		builder.Services.AddScoped<HttpClient>();
+		builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
